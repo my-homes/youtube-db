29fba0b28519e28b358c0910a067484bf51d7634
JavaCommons Technologies <javacommons@gmail.com>
2026-03-02 00:13:35 +0900

    # Commit at 2026.0302.0013.27 [DIFF] --- a/update-1080p.sh +++ b/update-1080p.sh -dir-multi.exe /p/@youtube-1080p | tee all/1080p.txt -truncate -s -1 all/1080p.txt -jq --raw-input --slurp 'split("\n")' all/1080p.txt | tee all/1080p-full-path.json +dir-multi.exe /p/@youtube-1080p | tee all/1080p.m3u +truncate -s -1 all/1080p.m3u +jq --raw-input --slurp 'split("\n")' all/1080p.m3u | tee all/1080p-full-path.json [/DIFF]

--- update-1080p.sh
+++ update-1080p.sh
@@ -6,7 +6,7 @@ cwd=`pwd`
 ts=`date "+%Y.%m%d.%H%M.%S"`
 
 mkdir -p all
-dir-multi.exe /p/@youtube-1080p | tee all/1080p.txt
-truncate -s -1 all/1080p.txt
-jq --raw-input --slurp 'split("\n")' all/1080p.txt | tee all/1080p-full-path.json
+dir-multi.exe /p/@youtube-1080p | tee all/1080p.m3u
+truncate -s -1 all/1080p.m3u
+jq --raw-input --slurp 'split("\n")' all/1080p.m3u | tee all/1080p-full-path.json
 ./.r.mk-1080p-list | tee all/1080p-list.json
