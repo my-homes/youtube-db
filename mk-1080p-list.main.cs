9b397560308dea9bb1a30a202b82afd099fce560
JavaCommons Technologies <javacommons@gmail.com>
2026-03-01 19:12:18 +0900

    # Commit at 2026.0301.1912.08 [DIFF] --- a/mk-1080p-list.main.cs +++ b/mk-1080p-list.main.cs -    ShowDetail = true; +    //ShowDetail = true; +    var all = FromFile("all/all.json"); -        string line = videoPath.Cast<string>(); -        Log(line); +        string fullPath = videoPath.Cast<string>(); +        Log(fullPath); -            line, +            fullPath, -        Log(m); +        //Log(m); +        if ( m != null ) +        { +            string id = m[1]; +            Log(id, "id"); +            var info = all[id]; +            info = info.Clone(hideKeys: ["detail"]); +            string videoDuration = info["duration"].Cast<string>(); +            TimeSpan duration = TimeSpan.Parse(videoDuration); +            var minutes = duration.TotalMinutes; +            info.Dynamic.minutes = minutes; +            info.Dynamic.path = fullPath; +            Log(info); +        } [/DIFF]

--- mk-1080p-list.main.cs
+++ mk-1080p-list.main.cs
@@ -6,21 +6,35 @@ using static Global.EasyObject;
 using static Global.Sys;
 try
 {
-    ShowDetail = true;
+    //ShowDetail = true;
     //SilentFlag = true;
     SetCwd(HomeFolder("youtube-db"));
+    var all = FromFile("all/all.json");
     var videoPathList = FromFile("all/1080p-full-path.json");
     Log(videoPathList);
     foreach(var videoPath in videoPathList.AsList!)
     {
-        string line = videoPath.Cast<string>();
-        Log(line);
+        string fullPath = videoPath.Cast<string>();
+        Log(fullPath);
         var m = Sys.FindFirstMatch(
-            line,
+            fullPath,
             @"\[([^\[\]]+)\][.]mp4$",
             @"【ID：([^【】]+)】[.]mp4$"
         );
-        Log(m);
+        //Log(m);
+        if ( m != null )
+        {
+            string id = m[1];
+            Log(id, "id");
+            var info = all[id];
+            info = info.Clone(hideKeys: ["detail"]);
+            string videoDuration = info["duration"].Cast<string>();
+            TimeSpan duration = TimeSpan.Parse(videoDuration);
+            var minutes = duration.TotalMinutes;
+            info.Dynamic.minutes = minutes;
+            info.Dynamic.path = fullPath;
+            Log(info);
+        }
     }
 }
 catch (Exception e)
